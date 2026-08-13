042c64b1fd65fdce9f516695283904a60c52afe0
JesusVertelB <vertelbravojesus@gmail.com>
2026-08-12 23:31:04 -0500

    Recuperar cambios del equipo

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sportia.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Verificar que haya iniciado sesión
+            int? idUsuario = HttpContext.Session.GetInt32("IdUsuario");
+            int? idRol = HttpContext.Session.GetInt32("IdRol");
+
+            // Si no ha iniciado sesión
+            if (idUsuario == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // Si no es cliente
+            if (idRol != 2)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
